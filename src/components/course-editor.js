a5f2eb8dd8e0b9afee567456162719dfc57de991
Mubaris Khan <mubaris.khan786@gmail.com>
2021-02-19 09:50:26 -0500

    adding skeleton for course table and course grid

---
+++ src/components/course-editor.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {Link} from "react-router-dom";
+
+
+const CourseEditor = ({history}) => {
+    return (
+        <h1>
+            <Link to="/courses">
+            <i className="fas fa-arrow-left"></i>
+            </Link>
+            <span onClick={() => history.goBack()}> Go back </span>
+            Course Editor!
+        </h1>
+    )
+}
+export default CourseEditor
